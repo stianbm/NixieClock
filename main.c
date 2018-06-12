0985e2fe3f3222b41227433c1a9f7c40f8dacc7c
stianbm <stian.malmanger@gmail.com>
2018-06-12 22:42:09 +0200

    Set up .gitignore

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * NixieClock.c
+ *
+ * Created: 12/06/2018 22:14:46
+ * Author : stianbm
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
