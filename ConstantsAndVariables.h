893c54e71c6ade00ab1d7fd158bbda8fb76fbc1f
stianbm <stian.malmanger@gmail.com>
2018-07-17 23:25:45 +0200

    Cleaned the code and added a header

---
+++ ConstantsAndVariables.h
@@ -0,0 +1,52 @@
+/*
+ * ConstantsAndVariables.h
+ *
+ * Created: 17/07/2018 19:32:01
+ *  Author: stian.malmanger
+ */ 
+
+
+#ifndef CONSTANTSANDVARIABLES_H_
+#define CONSTANTSANDVARIABLES_H_
+
+// Global constants
+static uint8_t maxSeconds = 59; 				// Max value for seconds counter
+static uint8_t maxMinutes = 59; 				// Max value for minutes counter
+static uint8_t maxHours = 23; 					// Max value for hours counter
+
+static uint8_t clearDigitPortA = 0b00000001;	// Mask to be and'ed to clear digits on port A
+static uint8_t clearDigitPortB = 0b00111110;	// Mask to be and'ed to clear digits on port B
+static uint8_t clearTubeSelect = 0b11110000;	// Mask to be and'ed to clear tube select on port C
+
+// Global variables
+static uint8_t hours = 0;						// Counter for hours
+static uint8_t minutes = 0;						// Counter for minutes
+static uint8_t seconds = 0;						// Counter for seconds
+
+static uint8_t digits[4] = {0, 0, 0, 0};		// Digits four hours and minutes
+
+static uint8_t hoursTemp = 0;					// Temp used in hoursToDigits()
+static uint8_t minutesTemp = 0;					// Temp used in minutesToDigits()
+
+// Pin numbers of tubes/transistors on port C - right to left / minutes to hours
+static uint8_t tubes[4] = {0b00000001,
+	0b00000010,
+	0b00000100,
+	0b00001000};
+
+// 8b variable where digit 0 and 1 are mode flags and digit 2 and 3 are "active tubes" flags
+static uint8_t modeFlags = 0b00000000;
+
+// Mode flag masks
+static uint8_t hourModeFlag = 0b00000010;
+static uint8_t minuteModeFlag = 0b00000010;
+
+// Pin masks:
+// Port B
+uint8_t zero = 0b00000001;
+uint8_t eight = 0b10000000;
+uint8_t nine = 0b01000000;
+// Port C
+uint8_t colon = 0b00100000;
+
+#endif /* CONSTANTSANDVARIABLES_H_ */
--- main.c
+++ main.c
@@ -4,202 +4,211 @@
  * Created: 12/06/2018 22:14:46
  * Author : stianbm
  *
+ *
  * The Nixie clock is supposed to show four digits, two for hours and two for minutes.
+ *
  * It also has two small Neon tubes to work as a colon, separating hours from minutes.
+ *
  * Two buttons are used for setting the time, press one to enter the mode, hour digits
  * blink, minutes don't.
+ *
  * Press other to increase hours, wraps around after 23.
+ *
  * Press first to set minutes, minute digits blink.
- * Press other to increase minutes, wrap around after	59.
+ *
+ * Press other to increase minutes, wrap around after 59.
+ *
  * Press first to return to normal operation.
+ *
  * Colon should toggle each second.
+ *
+ * Keep track of time in three variables: seconds, minutes and hours.
  * 
+ *
  * Based on the work of JoergenSteen @github and Atmel's START projects
  */ 
 
 #include <avr/io.h>								// A part of all AVR-programming for convenient programming
 #include <avr/interrupt.h>						// Atmel's own interrupt library
+#include "ConstantsAndVariables.h"				// Declarations of constants and variables used
 
 #define F_CPU 3333333UL							// The internal clock is 20Mhz, but it can't run that fast with 3,3V so it is prescaled by a factor of 6 to 3333333
 
 
-// Global variables
-unsigned long int seconds = 0;					// The main counter
-static unsigned long int maxSeconds = 86439; 	// 24 hours in seconds - 1		Have to use 32b
-static unsigned int secondsInHour = 3600;		// Seconds in an hour
-static unsigned int secondsInMinute = 60;		// Seconds in a minute
-
-unsigned int hours = 0;							// Counter for hours
-unsigned int minutes = 0;						// Counter for minutes
-unsigned int[] digits = {0, 0, 0, 0};			// Digits four hours and minutes
-
-unsigned int[] tubes = {0b11110001,
-	0b11110010,
-	0b11110100,
-	0b11111000};								// Pin numbers of tubes/transistors - right to left / minutes to hours
-
-bool buttonPressed = false;						// Flag for initial button press
-unsigned long int buttonSeconds = 0;			// Counter to keep track when button was pressed - used for initializing timeSetLoop
-bool setTimeMode = false;						// Flag to indicate if time is being set by user
-
-
-/* Calls the initializer and starts the update loop
- */
-int main(void)
-{
-    initializeGeneral();
-    updateLoop();
+void initializeCrystalAndPIT(){
+	// Deactivate change protection for register and start XOSC32K
+	// Start-up time is set to the maximum 64K cycles (~2 seconds)
+	// Use the macro "_PROTECTED_WRITE()"
+	_PROTECTED_WRITE(CLKCTRL_XOSC32KCTRLA, CLKCTRL_ENABLE_bm | CLKCTRL_RUNSTDBY_bm | CLKCTRL_CSUT_64K_gc);
+	// The RTC_STATUS needs to be 0 before writing to the RTC (could be used for start-up time).
+	while (RTC.STATUS != 0) {}
+	// Set crystal as clock source for RTC
+	RTC_CLKSEL = RTC_CLKSEL_TOSC32K_gc;
+	// Set the PIT periode to 32,768 cycles (1Hz) and enable PIT in RTC
+	RTC_PITCTRLA |= 0b01110000 | RTC_PITEN_bm;
+	// Enable PIT
+	RTC_PITINTCTRL |= 0x01;
+	
 }
 
-
-/* Enable interrupts, set output pins, set interrupt conditions, give priority  RTC, link interrupts to proper function
- */
+// Enable interrupts globally, set output pins, set interrupt conditions, link interrupts to proper function
 void initializeGeneral(){
 	// Set pins for digits, tubes and colon as output:
-	PORTA.DIR = 0xFF;
-	PORTB.DIR |= 0b11000001;
-	PORTC.DIR |= 0b100111;
+	PORTA.DIR = 0b11111110;
+	PORTB.DIR = 0b11000001;
+	PORTC.DIR = 0b00101111;
 	
-	PORTB.PIN1CTRL |= (1<<PORT_PULLUPEN_bp);  //Turns on the pull-up resistor on PORTB1/button1
-	PORTB.PIN1CTRL |= PORT_ISC_FALLING_gc;	// falling edge on PA5. This is preferred when the button is pulled up
+	/*
+
+	Not used for buttons not on devboard?
+
+	PORTB.PIN1CTRL |= (1<<PORT_PULLUPEN_bp);	// Turns on the pull-up resistor on PORTB1/button1
+	PORTB.PIN1CTRL |= PORT_ISC_FALLING_gc;		// falling edge on PA5. This is preferred when the button is pulled up
+	*/
 	
-	sei();  //turns on ISP(interrupt service routine).
+	// Turn on ISP(interrupt service routine) globally
+	sei();
+
+	initializeCrystalAndPIT();
 }
 
-
-/* Convert the second counter to hours and minutes, then to digits and then display them
- */
-void updateLoop(){
-	secondsToTime();
-	while(true){
-		// Display digits
-		writeToTubes();
-		
-		// Check if setTimeLoop should be entered
-		if(buttonPressed && seconds == buttonSeconds){
-			setTimeMode = true;
-			setTimeLoop();
-		}
-	}
-}
-
-
-/* Writes the digits to the tubes once per tube
- */
+// Writes the digits to the tubes once per tube - might have to add delay later
 void writeToTubes(){
-	for(unsigned int i = 0: i<4: i++){
-		// Set tube chooser pin tubes[i]
-		PORTC &= tubes[i];
+	for(uint8_t i = 0; i<4; i++){
+		// Clear and set tube chooser pin tubes[i]
+		PORTC.OUT &= clearTubeSelect;
+		PORTC.OUT |= tubes[i];
 		// Set tube pin digits[i]
-		if(digits[i] < 8){
-			PORTA = (1<<digits[i]);
+		if(digits[i] < 8 && digits[i] > 0){
+			PORTA.OUT &= clearDigitPortA;	// Clear digits on port A
+			PORTA.OUT |= (1<<digits[i]);
 		}
 		else{
+			PORTB.OUT &= clearDigitPortB;	// Clear digits on port B
 			switch(digits[i]){
-				case: 9{
-					PORTB &= 0b10111111;
+				case 9:
+					PORTB.OUT |= nine;
 					break;
-				}
-				case: 8{
-					PORTB &= 0b01111111;
+				case 8:
+					PORTB.OUT |= eight;
 					break;
-				}
+				case 0:
+					PORTB.OUT |= zero;
+					break;
 			}
 		}
 	}
 }
 
-
-/* Increments the seconds counter, called by time interrupt, also toggle Neon tubes for colon
- */
-void incrementTime(){
-	if(seconds == maxSeconds){
-		seconds = 0;
+// Splits hours into its two digits
+void hoursToDigits(){
+	hoursTemp = hours;
+	digits[3] = 0;
+	digits[2] = 0;
+	
+	while((hoursTemp - 10) >= 0){
+		hoursTemp -= 10;
+		digits[3]++;
 	}
-	else{
-		seconds++;
-	}
-	// Toggle the neon tubes
-	PORTC ^= 0b100000
+	digits[2] = hoursTemp;
 }
 
+void minutesToDigits(){
+	minutesTemp = minutes;
+	digits[1] = 0;
+	digits[0] = 0;
+	
+	while((minutesTemp - 10) >= 0){
+		minutesTemp -= 10;
+		digits[1]++;
+	}
+	digits[0] = minutesTemp;
+}
 
-/* Updates hours and minutes from the seconds counter, then updates the digit variables
- */
-void secondsToTime(){
-	unsigned long int secondsTemp = seconds;
-	hours = 0;
-	minutes = 0;
-	
-	while((secondsTemp - secondsInHour) >= 0){
-		secondsTemp -= secondsInHour;
-		hours++;
-	}
-	while((secondsTemp - secondsInMinute) >= 0){
-		secondsTemp -= secondsInMinute;
-		minutes++;
-	}
-	
+// Updates the digits
+void updateDigits(){
 	hoursToDigits();
 	minutesToDigits();
 }
 
-
-/* Splits hours into its two digits
- */
-void hoursToDigits(){
-	unsigned int hoursTemp = hours;
-	digits[0] = 0;
-	digits[1] = 0;
-	
-	while((hoursTemp - 10) >= 0){
-		hoursTemp -= 10;
-		digits[0]++;
+// Convert the second counter to hours and minutes, then to digits and then display them
+void updateLoop(){
+	while(1){
+		updateDigits();
+		writeToTubes();
 	}
-	digits[1] = hoursTemp;
 }
 
-
-/* Splits minutes into its two digits
- */
-void minutesToDigits(){
-	unsigned int minutesTemp = minutes;
-	digits[2] = 0;
-	digits[3] = 0;
-	
-	while((minutesTemp - 10) >= 0){
-		minutesTemp -= 10;
-		digits[2]++;
+void incrementHours(){
+	if(hours >= maxHours){
+		hours = 0;
 	}
-	digits[3] = minutesTemp;
+	else{
+		hours++;
+	}
 }
 
-
-/* Set a flag, return to updateLoop, wait for 2s, read button pin, if still pressed jump to setTimeLoop - is this the way?
- */
-void buttonInterruptHandler(){
-	
+void incrementMinutes(){
+	if(minutes >= maxMinutes){
+		minutes = 0;
+		incrementHours();
+	}
+	else{
+		minutes++;
+	}
 }
 
+void toggleColon(){
+	PORTC.OUT ^= colon;
+}
 
-/* Loop for blinking digits and changing values for seconds
- */
+// Increments the seconds counter, called by time interrupt, also toggle Neon tubes for colon
+void incrementSeconds(){
+	if(seconds >= maxSeconds){
+		seconds = 0;
+		incrementMinutes();
+	}
+	else{
+		seconds++;
+	}
+	// Toggle the colon neon tubes
+	toggleColon();
+}
+
+// Loop for blinking digits and changing values for hours and minutes
 void setTimeLoop(){
-	while(true){
+	while(1){
 		
 	}
 }
 
+// Interrupt handler for PIT, either increments seconds or toggle, depending on modeFlags
+ISR(RTC_PIT_vect){
+	if(!modeFlags){
+		incrementSeconds();
+	}
+	else{
+		blinkDigits();
+	}
+	// Reset interrupt flag
+	RTC_PITINTFLAGS |= 0xFF;
+}
+
+// Calls the initializer and starts the update loop
+int main(void){
+	initializeGeneral();
+	updateLoop();
+}
 
 
 /* TODO:
- * Make a loop for setting time
- * Enable time sensitive button interrupts in updateLoop - hold button for 3s
+ *
+ * Make a loop for setting time - solve how to toggle hour tubes and minute tubes
+ * Implement interrupt handlers for buttons
+ * Give priority to PIT
  */
 
-
 /* Pins and functions:
-
 GND
 VDD
 
@@ -227,3 +236,4 @@ PC2		TubeChoser 3
 PC3		TubeChoser 4
 PC4		Button Value
 PC5		Colon
+*/
